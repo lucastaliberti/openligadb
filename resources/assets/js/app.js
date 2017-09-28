01e7371dacce1418b7ce012bd1bd575a6fc7be4d
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-09-28 20:43:24 -0300

    added basic react scaffold

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,10 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-window.Vue = require('vue');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import Example from './components/Example';
 
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+render(<Example />, document.getElementById('root'));
