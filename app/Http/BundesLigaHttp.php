08a16e6037698c76d187a7c4d5022f3338e2cf4c
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2017-10-01 19:04:27 -0300

    Small HTTP Client for openligadb

---
+++ app/Http/BundesLigaHttp.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http;
+
+use GuzzleHttp;
+use Carbon\Carbon;
+
+class BundesLigaHttp
+{
+
+    var $client;
+
+    function __construct()
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'https://www.openligadb.de/api/',
+        ]);
+    }
+
+    public function request(string $method, string $path)
+    {
+        return $this->client->request($method, $path);
+    }
+
+    public function unwrappedRequestBody(string $method, string $path)
+    {
+        return GuzzleHttp\json_decode($this->request($method, $path)->getBody()->getContents());
+    }
+
+    public function getUpcomingMatches()
+    {
+        return array_filter($this->unwrappedRequestBody("GET", "getmatchdata/bl1/2017")
+            , function ($match)
+            {
+                $carbon = Carbon::parse($match->MatchDateTimeUTC, 'UTC');
+
+                return $carbon->isFuture();
+            });
+    }
+}
+
